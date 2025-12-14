aef7487b401c0ec440ed4df3d622eb5a509a49e1
Yazan Farah <yazan93farah@gmail.com>
2025-12-14 23:08:01 +0100

    fix now api uses env

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -6,14 +6,15 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { api } from "../../../environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class ApiInterceptor implements HttpInterceptor {
   intercept(
     req: HttpRequest<any>,
-    next: HttpHandler
+    next: HttpHandler,
   ): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `https://api.realworld.io/api${req.url}` });
+    const apiReq = req.clone({ url: `${api.apiUrl}${req.url}` });
     return next.handle(apiReq);
   }
 }
